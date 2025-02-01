febba29c423fbe2cd4bc06fb7bbcf47fbeda3f67
ryan pietersen <127750479+Ryan-Goosen@users.noreply.github.com>
2025-02-01 23:34:56 +0200

    changes

--- numeralconverter/__init__.py
+++ numeralconverter/__init__.py
@@ -4,6 +4,13 @@ from .numbered_base_convertion import from_base, to_base
 from .roman_convertion import from_roman, to_roman
 from .utils import is_valid_decimal
 
+"""
+The way this function is suppose to work is that a user adds a from and to base and we convert the number from to to. These bases can be almost anything so we should account for that.
+TODO: 
+- Fix the convertion process
+    - Allow the convertion process to be less static and work with more options.
+"""
+
 def convert(value:str, from_format:str, to_format:str) -> str:
     """
     Convert a numeral from one format to another.
@@ -13,9 +20,9 @@ def convert(value:str, from_format:str, to_format:str) -> str:
     value : str
         The numeral to convert.
     from_format : str
-        The current format of the numeral (e.g., "roman", "binary").
+        The current format of the numeral (e.g., "roman", "number_base").
     to_format : str
-        The target format (e.g., "morse", "binary").
+        The target format (e.g., "morse", "number_base").
 
     Returns
     -------
@@ -26,14 +33,14 @@ def convert(value:str, from_format:str, to_format:str) -> str:
     -----
     Currently Functional:
         - 'roman' -> ROMAN NUMERALS
-        - 'binary' -> BINARY
+        - 'number_base' -> CONVERTS NUMBER TO SELECTED BASE
     """
 
 
     converters = {
         "roman": (from_roman, to_roman),
         "decimal": (),
-        "binary":(from_base, to_base),
+        "number_base":(from_base, to_base),
     }
 
     if from_format.lower() not in converters or to_format.lower() not in converters:

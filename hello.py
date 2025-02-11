31a70e415ba1db4ce3b56b54d6ac9db201def998
ryan pietersen <127750479+Ryan-Goosen@users.noreply.github.com>
2025-02-11 20:48:32 +0200

    hello functions as a test platform for myself

---
+++ hello.py
@@ -0,0 +1,14 @@
+from numeralconverter import convert
+import math
+import string
+
+
+def main():
+    # variables = string.punctuation
+    # print(variables)
+    # numeral = convert(value="1000101", from_format="number_base", to_format="decimal")
+    print(convert(value='XXX', from_format="roman", to_format="number_base", base=9))
+    # print(from_base(numeral='98746320asdfghuytQWEZ55546', base=94))
+    # 407
+if __name__ == "__main__":
+    main()

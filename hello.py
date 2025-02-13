72a0702c4f4157abcdd43a01f1fc162e11843833
ryan pietersen <127750479+Ryan-Goosen@users.noreply.github.com>
2025-02-13 20:52:57 +0200

    updated the hello file

--- hello.py
+++ hello.py
@@ -1,14 +1,15 @@
+# MAIN FUNCTION
 from numeralconverter import convert
-import math
-import string
 
+# SUB FUNCTIONS USED INSIDE OF THE MAIN FUNCTION BUT CAN ALSO be CALLED BY THE USEr
+# FOR ROMAN CONVERTIONS
+from numeralconverter import from_roman, to_roman
+
+# FOR NUMERAL BASE CONVERTION
+from numeralconverter import from_base, to_base
 
 def main():
-    # variables = string.punctuation
-    # print(variables)
-    # numeral = convert(value="1000101", from_format="number_base", to_format="decimal")
     print(convert(value='XXX', from_format="roman", to_format="number_base", base=9))
-    # print(from_base(numeral='98746320asdfghuytQWEZ55546', base=94))
-    # 407
+
 if __name__ == "__main__":
     main()
